d3f7a2203d037342e107499a8a790276f9274f7f
rishab-bansal <73712021+rishab-bansal@users.noreply.github.com>
2026-04-14 11:33:57 +0200

    Websocket 3

--- script/main.js
+++ script/main.js
@@ -1,10 +1,15 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const ws = new WebSocket('wss://test-backend-7bl5.onrender.com/ws')
+const ws = new WebSocket('wss://test-backend-7bl5.onrender.com/ws');
 
+flag = true;
+if(flag){
 ws.onopen = () => {
-  ws.send('Hello from the client!')
+  console.log("connected");
+  ws.send('Hello from the client!');
 }
+}
+ws.onerror = (e)=>console.log("Error",e);
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
